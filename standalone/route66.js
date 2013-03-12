15947252dacf84d33016c10fc2614dd925b771cf
Guille Paz <guille87paz@gmail.com>
2013-03-12 09:13:30 -0300

    First commit. Create version 0.0.1

---
+++ standalone/route66.js
@@ -0,0 +1,178 @@
+(function (window) {
+    'use strict';
+
+    var location = window.location,
+        bind = window.addEventListener ? 'addEventListener' : 'attachEvent',
+        load = window.addEventListener ? 'load' : 'onload',
+        supported = (window.onpopstate !== undefined),
+        updateurl = supported ? 'popstate' : load;
+
+
+    /**
+     * Path Class
+     */
+    function Path(path) {
+        this.init(path);
+
+        return this;
+    }
+
+    Path.prototype.init = function (path) {
+        this.url = path;
+        this.listeners = [];
+        this.toRegExp();
+
+        return this;
+    };
+
+    Path.prototype.toRegExp = function () {
+        this.regexp = new RegExp('^' + this.url.replace(/:\w+/g, '([^\\/]+)').replace(/\//g, '\\/') + '$');
+
+        return this;
+    };
+
+
+    /**
+     * Route66 Class
+     */
+    function Route66() {
+        this.init();
+
+        return this;
+    }
+
+    Route66.prototype.init = function () {
+        var that = this,
+            hash;
+
+        this._collection = {};
+
+        window[bind](updateurl, function () {
+            hash = location.hash.split('#!')[1] || location.hash.split('#')[1];
+
+            // Home
+            if (location.pathname === '/' && hash === undefined) {
+                that._match('/');
+            } else {
+                that._match(hash);
+            }
+
+        }, false);
+
+        if (!supported) {
+            window[bind]('onhashchange', function () {
+                hash = location.hash.split('#!')[1] || location.hash.split('#')[1];
+                that._match(hash);
+            });
+        }
+
+        return this;
+
+    };
+
+    Route66.prototype._match = function (hash) {
+        var listeners,
+            key,
+            i = 0,
+            path,
+            params,
+            len;
+
+        for (key in this._collection) {
+
+            if (this._collection[key] !== undefined) {
+
+                path = this._collection[key];
+
+                params = hash.match(path.regexp);
+
+                if (params) {
+
+                    params.splice(0, 1);
+
+                    listeners = this._collection[key].listeners;
+
+                    len = listeners.length;
+
+                    for (i; i < len; i += 1) {
+                        listeners[i].apply(undefined, params);
+                    }
+                }
+
+            }
+        }
+
+        return this;
+    };
+
+    Route66.prototype.path = function (path, listener) {
+        var key;
+
+        if (typeof path === 'object' && listener === undefined) {
+
+            for (key in path) {
+                if (path[key] !== undefined) {
+                    this._createPath(key, path[key]);
+                }
+            }
+
+        } else {
+            this._createPath(path, listener);
+        }
+
+        return this;
+    };
+
+    Route66.prototype._createPath = function (path, listener) {
+        if (this._collection[path] === undefined) {
+            this._collection[path] = new Path(path);
+        }
+
+        this._collection[path].listeners.push(listener);
+    };
+
+    Route66.prototype.remove = function (path, listener) {
+        var listeners = this._collection[path],
+            i = 0,
+            len = listeners.length;
+
+        if (len !== undefined) {
+            for (i; i < len; i += 1) {
+                if (listeners[i] === listener) {
+                    listeners.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+        if (listeners.length === 0 || listener === undefined) {
+            delete this._collection[path];
+        }
+
+        return this;
+    };
+
+    Route66.prototype.paths = function (path) {
+        return (path !== undefined) ? this._collection[path] : this._collection;
+    };
+
+
+    /**
+     * Expose Route66
+     */
+    // AMD suppport
+    if (typeof window.define === 'function' && window.define.amd !== undefined) {
+        window.define('Route66', [], function () {
+            return Route66;
+        });
+
+    // CommonJS suppport
+    } else if (typeof module !== 'undefined' && module.exports !== undefined) {
+        module.exports = Route66;
+
+    // Default
+    } else {
+        window.Route66 = Route66;
+    }
+
+}(this));
